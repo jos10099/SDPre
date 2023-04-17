6a0f468a5b9be860349d89d22f3deaf07ee536ec
Jay Perkins <jul3jb@gmail.com>
2023-04-16 22:39:25 -0700

    complete Objective2Lab1

---
+++ Labs/Objective2Lab1.java
@@ -0,0 +1,8 @@
+public class Objective2Lab1 {
+	public static void main(String[] args) {
+		String hello = "Hello World";
+		System.out.println("\"" + hello + "\""); 
+		/*I was confused on whether or not you wanted quotations 
+		around the output*/
+	}
+}
