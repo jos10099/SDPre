d47a5bc6c59f8f51634cc6e138551de98b4fa3e0
Jay Perkins <jul3jb@gmail.com>
2023-04-17 11:25:46 -0700

    Complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Jay Perkins";
+		int age = 25;
+		String state = "Ohio";
+		System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+	}
+}
