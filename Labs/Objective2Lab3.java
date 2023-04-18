8dabfe1f214470fb455dc9c81743852aa8c44679
Jay Perkins <jul3jb@gmail.com>
2023-04-17 19:18:45 -0700

    Complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int birthYear = 1997;
+		String birthMonth = "May";
+		boolean isWeekend = false;
+		System.out.println(birthYear);
+		System.out.println(birthMonth);
+		System.out.println(isWeekend);
+	}
+}
