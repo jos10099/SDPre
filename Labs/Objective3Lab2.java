061eceeddac7946c0beba272dcc5d9d7c2c5aaca
Jay Perkins <jul3jb@gmail.com>
2023-04-18 12:22:31 -0700

    Complete3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,11 @@
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+		double num1,num2,hypotenuse; 
+		num1 = 5;
+		num2 = 8;
+
+		hypotenuse = Math.sqrt(num1 * num1 + num2 * num2);
+		System.out.println(hypotenuse);
+//I researched it and math.sqrt is a shorcut to find the square root.//
+	}
+}
