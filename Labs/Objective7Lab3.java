df36fd4603bd92fe95b4a8be3d2c15faebb49251
Jay Perkins <jul3jb@gmail.com>
2023-04-28 12:29:46 -0700

    Complete Objective7Lab3

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,17 @@
+public class Objective7Lab3 {
+	public static void main(String[] args) {
+		int counter = 1;
+
+		while(counter <=20) {
+			System.out.println(counter);
+
+			if(counter % 2 == 0) {
+				System.out.println(" is even");
+			}
+			else {
+				System.out.println( " is odd");
+			}
+			counter++;
+		}
+	}
+}
