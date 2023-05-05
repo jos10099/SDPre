01c293bfb51caa2e904d55bafa4465e68ffc8ab0
Jay Perkins <jul3jb@gmail.com>
2023-05-04 21:46:55 -0700

    Need help on Objective9Lab5.java

---
+++ Labs/Objective9Lab5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Objective9Lab5 {
+
+	public static void main(String[] args) {
+		Scanner kb = new Scanner (System.in);
+			double num1, num2;
+			boolean keepGoiong = true;
+			int choice;
+			double anwer = 0.0;
+
+			System.out.println("Please give me a number: ");
+				num1 = kb.nextDouble();
+
+			System.out.println("Please give me another number: ");
+				num2 = kb.nextDouble();
+
+		findSum = (num1 + num2)
+	}
+}
